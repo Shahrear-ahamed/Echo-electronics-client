47c9ae2a134de36d677def8fd50681aabbf520b1
Shahrear-ahamed <dev.shahrear@gmail.com>
2022-05-04 01:12:48 +0600

    send and verify user access token

--- src/Pages/MyItems/Myitems.js
+++ src/Pages/MyItems/Myitems.js
@@ -1,6 +1,8 @@
 import axios from "axios";
+import { signOut } from "firebase/auth";
 import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { toast } from "react-toastify";
 import auth from "../../firebase.init";
 import ManageSingleItem from "../ManageSingleItem/ManageSingleItem";
 
@@ -8,13 +10,33 @@ const Myitems = () => {
   const [user] = useAuthState(auth);
   const [myItmes, setMyItems] = useState([]);
   const { email } = user;
+  const [errorToast, setErrorToast] = useState("");
+
+  useEffect(() => {
+    if (!errorToast === "") {
+      toast.warning(errorToast);
+    }
+  }, [errorToast]);
 
   // find data from database
   useEffect(() => {
-    const url = `http://localhost:5000/inventory?email=${email}`;
-    axios(url).then((response) => {
-      setMyItems(response.data);
-    });
+    const url = `http://localhost:5000/singleuser?email=${email}`;
+    const verifyToken = async () => {
+      try {
+        const token = await axios.get(url, {
+          headers: {
+            authorization: `Bearer ${localStorage.getItem("access_token")}`,
+          },
+        });
+        setMyItems(token.data);
+      } catch (error) {
+        if (error.response.status === 401 || error.response.status === 403) {
+          signOut(auth);
+          setErrorToast(error.response.data.message);
+        }
+      }
+    };
+    verifyToken();
   }, [email]);
 
   return (
